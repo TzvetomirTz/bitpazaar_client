38f2942bdfdcef6eca55c7a1c164d9784d832115
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2024-01-11 21:17:37 +0200

    Should have hired a designer

---
+++ src/components/spacer/TopBarSpacer.jsx
@@ -0,0 +1,7 @@
+import './TopBarSpacer.css'
+
+function TopBarSpacer() {
+    return <div className='TopBarSpacer' />
+}
+
+export default TopBarSpacer;
--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -1,47 +1,49 @@
-import './ProfilePage.css';
-import TopBar from "../../components/topBar/TopBar";
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import ProfileClient from '../../clients/ProfileClient';
-import { authState } from '../../state/AuthState';
-import NftList from '../../components/nftList/NftList';
+import './ProfilePage.css'
+import TopBar from "../../components/topBar/TopBar"
+import TopBarSpacer from '../../components/spacer/TopBarSpacer'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import ProfileClient from '../../clients/ProfileClient'
+import { authState } from '../../state/AuthState'
+import NftList from '../../components/nftList/NftList'
 
 function ProfilePage() {
-  const { profileAddress } = useParams();
-  const walletConnected = authState((state) => state.walletConnected);
-  const accessKey = authState((state) => state.accessKey);
-  const [hmNftsOwned, setHmNftsOwned] = useState(0);
-  const [ownedNfts, setOwnedNfts] = useState([]);
+  const { profileAddress } = useParams()
+  const walletConnected = authState((state) => state.walletConnected)
+  const accessKey = authState((state) => state.accessKey)
+  const [hmNftsOwned, setHmNftsOwned] = useState(0)
+  const [ownedNfts, setOwnedNfts] = useState([])
 
   React.useEffect(() => {
     (async () => {
       if(walletConnected) {
-        loadProfileNfts();
+        loadProfileNfts()
       }
     })()
-  }, []);
+  }, [])
 
   const loadProfileNfts = async () => {
-    const nftData = await ProfileClient.getNftsByOwner(accessKey, "0x300e31AAF34aB0327Eaf6624C543Dbe19f44bbd3"); // ToDo: replace addr
-    setHmNftsOwned(nftData.nftsCount);
-    setOwnedNfts(nftData.nfts);
-  };
+    const nftData = await ProfileClient.getNftsByOwner(accessKey, "0x300e31AAF34aB0327Eaf6624C543Dbe19f44bbd3") // ToDo: replace addr
+    setHmNftsOwned(nftData.nftsCount)
+    setOwnedNfts(nftData.nfts)
+  }
 
   return (
     <div className='ProfilePage'>
       <TopBar />
+      <TopBarSpacer />
       <div className="ProfileContainer">
         <div className='ProfileHeader'>
           <div className='ProfilePic'></div>
           <div className='ProfileAddress'>{ profileAddress }</div>
         </div>
-        {/* <div className='ProfileBody'>
+        <div className='ProfileBody'>
           <div className='ProfileSectionTitle'>NFTS ({ hmNftsOwned }):</div>
           <NftList className="NftList" nfts={ ownedNfts } separateByCollection={true} />
-        </div> */}
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default ProfilePage;
+export default ProfilePage

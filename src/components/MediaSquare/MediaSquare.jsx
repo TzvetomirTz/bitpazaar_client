fdaccfc2d1c04cf3c955ffc29d0385a23fe87fb4
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2024-01-19 02:33:03 +0200

    Style the NFT visualisations inside the user profiles

--- src/components/MediaSquare/MediaSquare.jsx
+++ src/components/MediaSquare/MediaSquare.jsx
@@ -3,29 +3,21 @@ import './MediaSquare.css'
 function MediaSquare(props) {
     const { nft } = props
 
-    const displayThumbnail = () => {
-        if(nft.image.contentType !== undefined) {
-            if(nft.image.contentType.startsWith('image')) {
-                return <img className='Thumbnail' src={ nft.image.cachedUrl } />
-            } else if (nft.image.contentType === 'video/mp4') {
-                return <video src={ nft.image.cachedUrl } autoPlay={ true } loop={ true } className='Thumbnail' />
-            } else {
-                return <div></div>
-            }
-        } else if(nft.contract.openSeaMetadata !== undefined) {
-            const urlResized = nft.contract.openSeaMetadata.imageUrl.includes('=') ? nft.contract.openSeaMetadata.imageUrl.split('=')[0] + '=s200' : nft.contract.openSeaMetadata.imageUrl
-
-            return <img className='Thumbnail' src={ urlResized } />
+    if(nft.image.contentType !== undefined) {
+        if(nft.image.contentType.startsWith('image')) {
+            return <img className='MediaSquare' src={ nft.image.cachedUrl } />
+        } else if (nft.image.contentType === 'video/mp4') {
+            return <video src={ nft.image.cachedUrl } autoPlay={ true } loop={ true } className='MediaSquare' />
         } else {
             return <div></div>
         }
-    }
+    } else if(nft.contract.openSeaMetadata !== undefined) {
+        const urlResized = nft.contract.openSeaMetadata.imageUrl.includes('=') ? nft.contract.openSeaMetadata.imageUrl.split('=')[0] + '=s200' : nft.contract.openSeaMetadata.imageUrl
 
-    return (
-        <div className='MediaSquare'>
-            { displayThumbnail() }
-        </div>
-    )
+        return <img className='MediaSquare' src={ urlResized } />
+    } else {
+        return <div className='MediaSquare'></div>
+    }
 }
 
 export default MediaSquare
--- src/components/nftList/NftList.jsx
+++ src/components/nftList/NftList.jsx
@@ -5,9 +5,22 @@ function NftList(props) {
     const { nfts } = props
 
     const renderNfts = () => {
-        return nfts.map((n) => <div className='NftCard'>
-            
-        </div>)
+        return nfts.map((n) => {
+            const collectionName = n.contract.openSeaMetadata.collectionName !== undefined ?
+            n.contract.openSeaMetadata.collectionName : n.contract.name
+            const nftName = n.name !== undefined ? n.name : collectionName + " #" + n.tokenId
+        
+            return <div className='NftCard'>
+            <div className='NftCardVisualWrapper'>
+                <MediaSquare nft={n}/>
+            </div>
+            <div className='NftCardDetails'>
+                <div className='NftCardName'>{ nftName }</div>
+                <div className='NftCardCollectionName'>
+                    { collectionName }
+                </div>
+            </div>
+        </div>})
     }
 
     return (
